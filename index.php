a4d4cc5d1d731ccb2da1dda650992feb478b15e7
coque <coquelet.samuel@gmail.com>
2022-01-21 09:16:47 +0100

    fini

--- index.php
+++ index.php
@@ -9,9 +9,23 @@
  *    longueur de la chaîne de la troisième constante ( attention aux priorités )
  */
 // TODO Votre code ici.
+define('CONSTANTE', 12);
+define('CONSTANTE2', 2);
 
+echo CONSTANTE * CONSTANTE2."<br>";
+
+define('CONSTANTE3', "C'est PAAAAAAAAAAAAAAAAS cool PHP");
+echo (CONSTANTE + CONSTANTE2) * strlen(CONSTANTE3);
 
 /**
  * 6. Utilisez la bonne constante magique et éventuellement la bonne fonction pour afficher un maximum d'informations sur la page actuelle4
  */
 // TODO Votre code ici.
+echo basename(__FILE__)."<br>";
+echo basename(__DIR__)."<br>";
+echo basename(__LINE__)."<br>";
+echo basename(__FUNCTION__)."<br>";
+echo basename(__CLASS__)."<br>";
+echo basename(__METHOD__)."<br>";
+echo basename(__NAMESPACE__)."<br>";
+echo basename(__TRAIT__)."<br>";
